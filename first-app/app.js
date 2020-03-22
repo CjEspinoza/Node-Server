8ba353033fa91f64f87d21092663b3c2eddee9bc
CjEspinoza <espinoza.cj3@gmail.com>
2020-03-22 19:52:07 +0800

    add css

--- first-app/app.js
+++ first-app/app.js
@@ -1,15 +1,37 @@
+var http = require("http"),
+    url = require("url"),
+    path = require("path"),
+    fs = require("fs"),
+    port = process.argv[2] || 8888;
 
-const http = require('http');
+http.createServer(function(request, response) {
 
-const  server = http.createServer((req, res)=>{
-    if(req.url === '/'){
-        res.sendFile(path.join(__dirname, '/SimpleContactForm.html'));
-        res.end();
+  var uri = url.parse(request.url).pathname
+    , filename = path.join(process.cwd(), uri);
+
+  fs.exists(filename, function(exists) {
+    if(!exists) {
+      response.writeHead(404, {"Content-Type": "text/plain"});
+      response.write("404 Not Found\n");
+      response.end();
+      return;
     }
-});
 
+    if (fs.statSync(filename).isDirectory()) filename += '/SimpleContactForm.html';
 
+    fs.readFile(filename, "binary", function(err, file) {
+      if(err) {        
+        response.writeHead(500, {"Content-Type": "text/plain"});
+        response.write(err + "\n");
+        response.end();
+        return;
+      }
 
-server.listen(3000);
+      response.writeHead(200);
+      response.write(file, "binary");
+      response.end();
+    });
+  });
+}).listen(parseInt(port, 10));
 
-console.log('Listening on port 3000...');
+console.log("Static file server running at\n  => http://localhost:" + port + "/\nCTRL + C to shutdown");
