b0b03dc83951159279324aa4ca35357ed75e87f9
CjEspinoza <espinoza.cj3@gmail.com>
2020-03-22 19:31:52 +0800

    simplecontactform

--- first-app/app.js
+++ first-app/app.js
@@ -1,12 +1,9 @@
+
 const http = require('http');
 
 const  server = http.createServer((req, res)=>{
     if(req.url === '/'){
-        res.write('Hello World');
-        res.end();
-    }
-    if(req.url === '/api/courses'){
-        res.write(JSON.stringify([1,2,3]));
+        res.sendFile(path.join(__dirname, '/SimpleContactForm.html'));
         res.end();
     }
 });
